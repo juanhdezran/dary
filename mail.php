f64c01103bbf8592316ffa6f15d02db3c84b5390
Juan Hernández <juanhdezran@gmail.com>
2018-05-29 23:28:08 -0500

    mail para angular

--- mail.php
+++ mail.php
@@ -1,12 +1,9 @@
 <?php
 header('Access-Control-Allow-Headers: Content-Type');
-//header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Origin: *');
-
 $data = json_decode(file_get_contents("php://input"), true);
-$frames = json_decode($data['marcos'], true); 
-
-if(isset($data['nombre'])){
+$frames = json_decode($data['marcos'], true);
+if(isset($data['nombre'])){       
     $body = '<html><body>';
     $body .= '<table style="width: 500px; margin: 0 auto;">';
     $body .= '<tr><th colspan="4" style="text-align:center; background-color: #e2e2e2;">Solicitud de Cotizacion</th></tr>';
@@ -22,12 +19,12 @@ if(isset($data['nombre'])){
     $body .= '<td colspan="3" style="padding-left: 12px">' . $data['estado'] . '</td></tr>';
     $body .= '<tr><th style="text-align:right;">Comentarios:</th>';
     $body .= '<td colspan="3" style="padding-left: 12px">' . $data['comentarios'] . '</td></tr>';
-    $body .= '<tr><td colspan="4">&nbsp;</td></tr>'
+    $body .= '<tr><td colspan="4">&nbsp;</td></tr>';
     $body .= '<tr><th style="text-align:left">Cantidad</th><th style="text-align:left">Descripcion</th><th style="text-align:left">Precio U.</th><th style="text-align:left">Importe</th></tr>';
     $total = 0;
     foreach ($frames as $key => $value) {
         foreach ($value['detail'] as $key2 => $value2) {
-            if($value['checked']){
+            if($value2['checked']){
                 $body .= '<tr><td>' . $value2['quantity'] . '</td>';
                 $body .= '<td>' . $value['model'] . ' ' . $value2['width'] .' x '. $value2['height'] .'</td>';
                 $body .= '<td>' . $value2['price'] . '</td>';
@@ -44,11 +41,12 @@ if(isset($data['nombre'])){
     $headers   = '';
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= "From: estudio@fotodary.com". "\r\n";;
-    $headers .= "Reply-To: estudio@fotodary.com". "\r\n";;
+    $headers .= "From: estudio@fotodary.com". "\r\n";
+    $headers .= "Reply-To: estudio@fotodary.com". "\r\n";
     $headers .= "X-Mailer: PHP/".phpversion();
 
-    $res = mail('juanhdezran@gmail.com', 'Cotizacion', $body, $headers);
+    $res = mail('fotodary@hotmail.com', 'Cotizacion', $body, $headers);
     return $res;
 }
+
 
